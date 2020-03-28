5e306689e0936c8a5bcca399d534d5cea69645bb
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2020-03-28 09:32:43 -0400

    dpv_documento:: agrada estructura de archivos para la inclucion de la app por parte del sistema, se agrego la configuracion del __init__.py, apps.py y la carpeta para template de menu y los archivos estaticos

--- apps/dpv_documento/apps.py
+++ apps/dpv_documento/apps.py
@@ -2,4 +2,17 @@ from django.apps import AppConfig
 
 
 class DpvDocumentoConfig(AppConfig):
-    name = 'dpv_documento'
+    name = 'apps.dpv_documento'  # Nombre identificador del modulo tiene que cumplir con el estandar
+    verbose_name = "Documentos"  # Nombre de Humanizado del modulo sera utilizado para mostrar en el menu
+    menu = 'dpv_documento/menu/main_menu.html'  # Plantilla HTML de que se mostrara el modulo del menu
+    menuable = True  # Si el modulo se mostrara en el menu
+    owned = True  # Si es una aplicacion nuestra
+    active = True  # Si el modulo esta activo, es como si esta instalado
+    parent = True  # Si el modulo es un submodulo de otro en el menu (y solo en el menu)
+    child_of = ''  # Si el modulo es hijo de otro en el menu se coloca aqui el nombre(atributo name) del modulo padre
+    model_data = ['Documento']
+    name_data = 'Documentos Registrados'  # Nombre o texto a mostrar del sisginificado de dichos registros
+    route_data = 'documentos_index'  # ruta principal del modulo
+
+    def ready(self):
+        pass
