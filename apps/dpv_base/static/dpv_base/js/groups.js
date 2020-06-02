458a18f53fe34c4a069bb5717e2afc766a420edb
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2020-06-02 01:07:07 -0400

    dpv_base:: agregado el codigo js en el archivo groups.js para validar el nombre de los grupos y el html necesario en el archivo plantilla groups_form.html par que suceda la vlaidación

--- apps/dpv_base/static/dpv_base/js/groups.js
+++ apps/dpv_base/static/dpv_base/js/groups.js
@@ -13,3 +13,102 @@ $(document).ready(function(){
     });
 
 });
+
+
+const DPVGrupo = function () {
+    let group_form;
+    let tmp;
+    let validator_form;
+
+    const _makeAlert = function(type, text, success, negate, title=null, accept=null, cancel=null, plus=null) {
+        let dataplus = plus || tmp;
+        Swal.fire({
+            title: title || "Error",
+            html: text,
+            icon: type,
+            showCloseButton: true,
+            showCancelButton: true,
+            focusConfirm: false,
+            confirmButtonText: accept || '<i class="fa fa-thumbs-up"></i> Si',
+            confirmButtonAriaLabel: 'Correto',
+            cancelButtonText: cancel || '<i class="fa fa-thumbs-down"></i> No',
+            cancelButtonAriaLabel: 'Cancelar',
+            showClass: {
+                popup: 'animated fadeInDown faster',
+            },
+            hideClass: {
+                popup: 'animated fadeOutUp faster',
+            },
+        }).then((result) => {
+            if (result.value) {
+                if (success != null) {
+                    if (typeof(success) == 'function')
+                        success(dataplus);
+                }
+            } else {
+                if (negate != null) {
+                    negate(dataplus);
+                }
+            }
+        });
+    };
+    const _initGroupPane = function () {
+
+    };
+    const _initGroupForm = function () {
+
+        $.validator.setDefaults({
+            errorClass: 'text-danger',
+            highlight: function(element) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function(element) {
+                $(element).removeClass('is-invalid');
+            },
+
+			ignore: ":hidden",
+
+            errorPlacement: function(error, element) {
+                if (element[0].attributes['type'].nodeValue === 'select-one' || element[0].attributes['type'].nodeValue === 'select-multiple')
+                    error.insertBefore(element.parent());
+                else
+                    error.insertBefore(element);
+            },
+
+        });
+
+        validator_form = group_form.validate({
+			rules: {
+                name: {
+				    required: true,
+                    maxlength: 130,
+                    remote: {
+                        url: '/admin/group/verify/',
+                        type: 'GET',
+                        data: {
+                            id: grp_id,
+                        },
+                    },
+                },
+			},
+			messages: {
+                name: {
+				    required: "El nombre no puede quedar en blanco.",
+                    maxlength: "El nombre no puede contener más de 130 caracteres.",
+                    remote: "El nombre es único y ya existe otro grupo registrado con ese nombre.",
+                },
+			},
+		});
+
+    };
+
+    return {
+        init: function () {
+            _initGroupPane();
+        },
+        initForm: function () {
+            group_form = $("#grp-form");
+            _initGroupForm();
+        },
+    };
+}();
