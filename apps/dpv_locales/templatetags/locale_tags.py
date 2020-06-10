1131fc5214d40204e0bf57557090b3572be98b87
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2020-06-10 01:23:20 -0400

    dpv_locales:: templatetags::locale_tags:: agregados los tags is_list e is_queryset para validar que un dato en la plantilla es una lista o un queryset, corregidos detalles en el tag valuetoint.

--- apps/dpv_locales/templatetags/locale_tags.py
+++ apps/dpv_locales/templatetags/locale_tags.py
@@ -1,18 +1,31 @@
 from django import template
-
+from django.db.models import QuerySet
 
 register = template.Library()
 
 
 @register.filter
 def valuetoint(value):
-    '''
+    """
     Return the value converted to int if posibile
     Returns empty string if some error
-    '''
+    """
     try:
         value = int(value)
         return value
-    except:
-        pass
+    except Exception as e:
+        print(e)
+        if not value:
+            return 0
     return value
+
+
+@register.filter
+def is_list(value):
+
+    return isinstance(value, list)
+
+
+@register.filter
+def is_queryset(value):
+    return isinstance(value, QuerySet)
