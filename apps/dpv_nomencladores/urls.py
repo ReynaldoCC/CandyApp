6d6ecb09edaa80d0b7de51814dc4b9dd0afaf247
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2020-07-25 22:27:02 -0400

    dpv_nomencladores::urls:: agregadas las rutas para acceder a las funcionalidades verify_provincia, verify_consejopopular, verify_calle, verify_piso, verify_destino, verify_concepto, verify_areatrabajo, verify_centrotrabajo, verify_codificadorasunto, verify_tipoqueja,verify_tipoprocedencia,   verify_estado, verify_clasificacionrespuesta.

--- apps/dpv_nomencladores/urls.py
+++ apps/dpv_nomencladores/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 
     path('provincia/', index_provincia, name='nomenclador_provincia'),
     path('nueva_provincia/', add_provincia, name='provincia_new'),
+    path('verify_provincia/', verify_provincia, name='provincia_verify'),
     path('editar_provincia/<int:id_provincia>', update_provincia, name='provincia_edit'),
     path('eliminar_provincia/<int:id_provincia>', delete_provincia, name='provincia_delete'),
 
@@ -18,6 +19,7 @@ urlpatterns = [
     path('consejopopular/', index_consejopopular, name='nomenclador_consejopopular'),
     path('consejopopular/filter/<int:id_municipio>', filter_by_muncp, name='nomenclador_consejopopular'),
     path('nuevo_consejopopular/', add_consejopopular, name='consejopopular_new'),
+    path('verify_consejopopular/', verify_consejopopular, name='consejopopular_verify'),
     path('consejopopular/add/', add_cpopular_json, name='consejopopular_add'),
     path('editar_consejopopular/<int:id_consejopopular>', update_consejopopular, name='consejopopular_edit'),
     path('eliminar_consejopopular/<int:id_consejopopular>', delete_consejopopular, name='consejopopular_delete'),
@@ -25,6 +27,7 @@ urlpatterns = [
     path('calle/', index_calle, name='nomenclador_calle'),
     path('calle/filter/<int:id_municipio>', filter_by_municipio, name='filter_calle'),
     path('nueva_calle/', add_calle, name='calle_new'),
+    path('verify_calle/', verify_calle, name='calle_verify'),
     path('new_calle/', add_calle_on_user, name='calle_new_async'),
     path('agree_calle/<str:select_id>', agree_calle, name='agree_calle'),
     path('calle/add/', add_calle_json, name='add_calle'),
@@ -33,6 +36,7 @@ urlpatterns = [
 
     path('piso/', index_piso, name='nomenclador_piso'),
     path('nuevo_piso/', add_piso, name='piso_new'),
+    path('verify_piso/', verify_piso, name='piso_verify'),
     path('editar_piso/<int:id_piso>', update_piso, name='piso_edit'),
     path('eliminar_piso/<int:id_piso>', delete_piso, name='piso_delete'),
 
@@ -45,36 +49,43 @@ urlpatterns = [
 
     path('destino/', index_destino, name='nomenclador_destino'),
     path('nuevo_destino/', add_destino, name='destino_new'),
+    path('verify_destino/', verify_destino, name='destino_verify'),
     path('editar_destino/<int:id_destino>', update_destino, name='destino_edit'),
     path('eliminar_destino/<int:id_destino>', delete_destino, name='destino_delete'),
 
     path('concepto/', index_concepto, name='nomenclador_concepto'),
     path('nuevo_concepto/', add_concepto, name='concepto_new'),
+    path('verify_concepto/', verify_concepto, name='concepto_verify'),
     path('editar_concepto/<int:id_concepto>', update_concepto, name='concepto_edit'),
     path('eliminar_concepto/<int:id_concepto>', delete_concepto, name='concepto_delete'),
 
     path('genero/', index_genero, name='nomenclador_genero'),
     path('nuevo_genero/', add_genero, name='genero_new'),
+    path('verify_genero/', verify_genero, name='genero_verify'),
     path('editar_genero/<int:id_genero>/', update_genero, name='genero_edit'),
     path('eliminar_genero/<int:id_genero>', delete_genero, name='genero_delete'),
 
     path('departamento/', index_areatrabajo, name='nomenclador_areatrab'),
     path('nuevo_departamento/', add_areatrabajo, name='areatrabajo_new'),
+    path('verify_departamento/', verify_areatrabajo, name='areatrabajo_verify'),
     path('editar_departamento/<int:id_areatrabajo>', update_areatrabajo, name='areatrabajo_edit'),
     path('eliminar_departamento/<int:id_areatrabajo>', delete_areatrabajo, name='areatrabajo_delete'),
 
     path('unidad/', index_centrotrabajo, name='nomenclador_centrab'),
-    path('nuevo_centrotrabajo/', add_centrotrabajo, name='centrotrabajo_new'),
+    path('nueva_unidad/', add_centrotrabajo, name='centrotrabajo_new'),
+    path('verify_unidad/', verify_centrotrabajo, name='centrotrabajo_verify'),
     path('editar_unidad/<int:id_centrotrabajo>', update_centrotrabajo, name='centrotrabajo_edit'),
     path('eliminar_unidad/<int:id_centrotrabajo>', delete_centrotrabajo, name='centrotrabajo_delete'),
 
     path('codificadorasunto/', index_codificadorasunto, name='nomenclador_codificadorasunto'),
     path('nuevo_codificadorasunto/', add_codificadorasunto, name='codificadorasunto_new'),
+    path('verify_codificadorasunto/', verify_codificadorasunto, name='codificadorasunto_verify'),
     path('editar_codificadorasunto/<int:id_codificadorasunto>', update_codificadorasunto, name='codificadorasunto_edit'),
     path('eliminar_codificadorasunto/<int:id_codificadorasunto>', delete_codificadorasunto, name='codificadorasunto_delete'),
 
     path('tipoqueja/', index_tipoqueja, name='nomenclador_tipoqueja'),
     path('nuevo_tipoqueja/', add_tipoqueja, name='tipoqueja_new'),
+    path('verify_tipoqueja/', verify_tipoqueja, name='tipoqueja_verify'),
     path('editar_tipoqueja/<int:id_tipoqueja>', update_tipoqueja, name='tipoqueja_edit'),
     path('eliminar_tipoqueja/<int:id_tipoqueja>', delete_tipoqueja, name='tipoqueja_delete'),
 
@@ -85,16 +96,19 @@ urlpatterns = [
 
     path('tipoprocedencia/', index_tipoprocedencia, name='nomenclador_tipoprocedencia'),
     path('nuevo_tipoprocedencia/', add_tipoprocedencia, name='tipoprocedencia_new'),
+    path('verify_tipoprocedencia/', verify_tipoprocedencia, name='tipoprocedencia_verify'),
     path('editar_tipoprocedencia/<int:id_tipoprocedencia>', update_tipoprocedencia, name='tipoprocedencia_edit'),
     path('eliminar_tipoprocedencia/<int:id_tipoprocedencia>', delete_tipoprocedencia, name='tipoprocedencia_delete'),
 
     path('estado/', index_estado, name='nomenclador_estado'),
     path('nuevo_estado/', add_estado, name='estado_new'),
+    path('verify_estado/', verify_estado, name='estado_verify'),
     path('editar_estado/<int:id_estado>', update_estado, name='estado_edit'),
     path('eliminar_estado/<int:id_estado>', delete_estado, name='estado_delete'),
 
     path('clasificacionrespuesta/', index_clasificacionrespuesta, name='nomenclador_clasificacionrespuesta'),
     path('nuevo_clasificacionrespuesta/', add_clasificacionrespuesta, name='clasificacionrespuesta_new'),
+    path('verify_clasificacionrespuesta/', verify_clasificacionrespuesta, name='clasificacionrespuesta_verify'),
     path('editar_clasificacionrespuesta/<int:id_clasificacionrespuesta>', update_clasificacionrespuesta, name='clasificacionrespuesta_edit'),
     path('eliminar_clasificacionrespuesta/<int:id_clasificacionrespuesta>', delete_clasificacionrespuesta, name='clasificacionrespuesta_delete'),
 
