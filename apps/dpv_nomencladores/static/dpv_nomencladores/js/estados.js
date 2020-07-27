eb2dbcf8523bbc2892aed6946d96e1d131b5a1e0
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2020-07-27 01:03:53 -0400

    dpv_nomencladores::templates::list_estado:: agregada la carga de tiquetas de idioma con i18n, la carga de srchivos js y css para selectize, cambiado el atributo id de la table par su posterior incializacion. Inicalizacion con las traducciones para el datatables dpv_nomencladores::templates::form_estado:: agregada inicializacion del formulario mediante el objeto DPVEstadoNom. dpv_nomencladores::static::js::estados:: agregada codigo del objeto DPVEstadoNom listo para las funcionalidades de listar y formulario.

---
+++ apps/dpv_nomencladores/static/dpv_nomencladores/js/estados.js
@@ -0,0 +1,110 @@
+'use strict';
+
+function abrir_modal(url, id=null)
+{
+    $('#popup').load(url, function()
+    {
+        $(this).modal('show');
+    });
+
+    return false;
+}
+
+function cerrar_modal()
+{
+    $('#popup').modal('hide');
+    return false;
+}
+
+var DPVEstadoNom =  function () {
+    let estado_form;
+    let validator_form;
+
+    const _initEstadoPane = function (translations) {
+        $('#estado-table').DataTable({
+            responsive: true,
+            order: [ 0, 'desc' ],
+            lengthMenu: [20, 35, 50, "All"],
+            sScrollX: "100%",
+            language: {
+                "decimal": "",
+                "emptyTable": translations.emptyTable,
+                "info": translations.info_init + " _START_ a _END_ de _TOTAL_ " + translations.info_end,
+                "infoEmpty": translations.infoEmpty,
+                "infoFiltered": "(" + translations.infoFiltered_init + " _MAX_ " + translations.infoFiltered_end + ")",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": translations.lengthMenu_init + " _MENU_ " + translations.lengthMenu_end,
+                "loadingRecords": translations.loadingRecords,
+                "processing": translations.processing,
+                "search":  translations.search,
+                "zeroRecords": translations.zeroRecords,
+                "paginate": {
+                        "first": translations.first,
+                        "last": translations.last,
+                        "next": translations.next,
+                        "previous": translations.previous,
+                }
+            },
+        });
+    };
+    const _initEstadoForm =  function () {
+
+        $.validator.setDefaults({
+            errorClass: 'text-danger',
+            highlight: function(element) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function(element) {
+                $(element).removeClass('is-invalid');
+            },
+
+			ignore: ":hidden",
+
+            errorPlacement: function(error, element) {
+                if (element[0].attributes['type'].nodeValue === 'select-one' || element[0].attributes['type'].nodeValue === 'select-multiple')
+                    error.insertBefore(element.parent());
+                else
+                    error.insertBefore(element);
+            },
+
+        });
+        $.validator.addMethod("letterswithbasicpuncandspace", function(value, element) {
+            return this.optional(element) || /^[a-zA-Z0-9áéíóúÁÉÚÍÓñÑ \-.,()'"\s]+$/i.test(value);
+        }, "solo puede tener letras, números, y signos de puntuación básicos");
+        validator_form = estado_form.validate({
+			rules: {
+				nombre: {
+				    maxlength: 90,
+				    required: true,
+                    letterswithbasicpuncandspace: true,
+                    remote: {
+                        url: '/nomenclador/verify_estado/',
+                        type: 'GET',
+                        data: {
+                            id: estado_id,
+                        },
+                    },
+				},
+			},
+			messages: {
+				nombre: {
+				    maxlength: "El nombre del estado de la queja no puede tener más de 90 caracteres.",
+				    required: "El nombre del estado de la queja es obligatorio.",
+                    letterswithbasicpuncandspace: "El nombre del estado de la queja solo puede tener letras, números, y signos de puntuación básicos.",
+                    remote: "Ya existe otro estado de la queja registrado con ese nombre.",
+				},
+			},
+		});
+    };
+
+    return {
+        init: function (translations) {
+            _initEstadoPane(translations);
+        },
+        initForm: function () {
+            estado_form = $('#form_estado');
+            _initEstadoForm();
+        },
+    }
+}();
