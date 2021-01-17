8c98e7ebdf2654abbe9208b20ca5e1bf3e4b484a
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2021-01-17 00:07:34 -0500

    cambiada la configuracionde celery

--- locales_viv/celery.py
+++ locales_viv/celery.py
@@ -8,7 +8,7 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locales_viv.settings')
 
 celery_app = Celery('locales_viv', namespace='CELERY')
-# celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
 
 
