1ff56e34704c2bc6e1b654619869c223ae0da114
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2021-02-17 12:15:48 -0500

    cambiado el valor de la varible DJANGO_SETTINGS_MODULE y agregado al path las aplicaciones(modulos)

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+from pathlib import Path
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -12,4 +13,8 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    current_path = Path(__file__).parent.resolve()
+    sys.path.append(str(current_path / "apps"))
+
     execute_from_command_line(sys.argv)
