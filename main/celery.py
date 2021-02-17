ae0a9875e47e37c64b010e0496c699898c6368e1
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2021-02-17 14:16:32 -0500

    cambiada la configuracion para que use una varible de entorno para DJANGO_SETTINGS_MODULE o el modulo de producion por defecto

--- main/celery.py
+++ main/celery.py
@@ -1,13 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
+import environ
 
 from celery import Celery
 
+env = environ.Env()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', env.str("DJANGO_SETTINGS_MODULE", default='main.settings.production'))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
-celery_app = Celery('locales_viv', namespace='CELERY')
+celery_app = Celery('main', namespace='CELERY')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
 
