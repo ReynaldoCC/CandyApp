0093d2d0e6bb51329cd6e06cf78e5b5873d81dbd
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2021-02-17 12:10:39 -0500

    cambiada la importacion de settigns que estaba desde el archivo settings y ahora se importa desde django.conf, otros cambios cosmeticos

--- apps/dpv_base/templatetags/base_tags.py
+++ apps/dpv_base/templatetags/base_tags.py
@@ -2,22 +2,24 @@ from django import template
 from django.apps import apps as all_apps
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import SafeData, mark_safe
-from main import settings
+from django.conf import settings
 
 register = template.Library()
 
+
 @register.simple_tag
 def local_apps():
-    local_apps = []
+    local_installed_apps = []
     if all_apps.get_app_configs:
         for app in all_apps.get_app_configs():
             if hasattr(app, 'owned') and app.active:
-                local_apps.append(app)
-    return local_apps
+                local_installed_apps.append(app)
+    return local_installed_apps
+
 
 @register.simple_tag
 def menuable_apps():
-    menuable_apps = []
+    menuable_installed_apps = []
     if all_apps.get_app_configs:
         for app in all_apps.get_app_configs():
             if hasattr(app, 'owned') and app.active:
@@ -26,26 +28,29 @@ def menuable_apps():
                         if len(app.model_data) == 1:
                             setattr(app, 'count_data', app.get_model(app.model_data[0]).objects.all().count() or 0)
                         else:
-                            setattr(app, 'count_data', all_apps.get_model(app.model_data[0], app.model_data[1]).objects.all().count() or 0)
-                    menuable_apps.append(app)
-    return menuable_apps
+                            setattr(app,
+                                    'count_data',
+                                    all_apps.get_model(app.model_data[0],
+                                                       app.model_data[1]).objects.all().count() or 0)
+                    menuable_installed_apps.append(app)
+    return menuable_installed_apps
+
 
 @register.simple_tag()
 def exist_url(url=None):
     if not url:
         return False
-    # for urlpat in settings.BULK_URLS:
-    #
-    #     if url in urlpat.name:
-    #         return True
     return settings.BULK_URLS
+
 
 @register.simple_tag()
 def ative_url(url=None):
     if not url:
         return False
-    for urlpath in settings.BULK_URLS:
-        pass
+    if url in settings.BULK_URLS:
+        return True
+    return False
+
 
 @register.filter
 @stringfilter
@@ -58,12 +63,13 @@ def split(value, arg):
         return mark_safe(value)
     return value
 
+
 @register.filter
-def restof( value, arg):
-    '''
+def restof(value, arg):
+    """
     Divides the value; the argument is the divisor and it is ok return rest of division
     Returns empty string if some error
-    '''
+    """
     try:
         value = int(value)
         arg = int(arg)
@@ -76,10 +82,10 @@ def restof( value, arg):
 
 @register.filter
 def valuetoint(value):
-    '''
+    """
     Return the value converted to int if posibile
     Returns empty string if some error
-    '''
+    """
     try:
         value = int(value)
         return value
@@ -92,10 +98,10 @@ def valuetoint(value):
 
 @register.filter
 def tosub(value, arg):
-    '''
+    """
     Sub this values and args if is posibile
     Returns empty string if some error
-    '''
+    """
     try:
         value = int(value)
         arg = int(arg)
