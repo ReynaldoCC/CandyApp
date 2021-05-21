cd924f8a73867ff00fb15707cdb7d31171baee6e
Reynaldo Cuenca Campos (masquer) <reynaldo86.onwork@gmail.com>
2021-05-21 16:16:43 -0400

    cambios cosmeticos

--- main/celery.py
+++ main/celery.py
@@ -11,5 +11,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', env.str("DJANGO_SETTINGS_MODULE"
 celery_app = Celery('main', namespace='CELERY')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
-
-
